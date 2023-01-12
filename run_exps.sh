48fd641199fb7a6e26499c9cca4f3aab9d4bb116
skezle <kessler.s.c@gmail.com>
2023-01-12 21:11:06 +0000

    Update run_exps.sh

--- run_exps.sh
+++ run_exps.sh
@@ -1,5 +1,5 @@
 for seed in 0 1 2 3 4 5 6 7 8 9
 do
-  OMP_NUM_THREADS=1 python main.py --config-file configs/minihack/clear_minihack_small.json --resume-id ${seed}
-  OMP_NUM_THREADS=1 python main.py --config-file configs/minihack/impala_minihack_small.json --resume-id ${seed}
+  OMP_NUM_THREADS=1 python main.py --config-file configs/minihack/clear_minihack_small.json --resume-id ${seed} --name clear_cl_small --group minihack
+  OMP_NUM_THREADS=1 python main.py --config-file configs/minihack/impala_minihack_small.json --resume-id ${seed} --name impala_cl_small --group minihack
 done
